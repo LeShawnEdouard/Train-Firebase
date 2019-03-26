9763f05241a851b3f38c01231c9720f6dc1b2fd8
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-03-25 23:48:16 -0400

    Added firebase API key to js file. Changed headers on skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+var config = {
+  apiKey: "AIzaSyCCGquCQzFe8bddfYtyw6WlBUuh3DuPjOY",
+  authDomain: "train-scheduler-6b3c6.firebaseapp.com",
+  databaseURL: "https://train-scheduler-6b3c6.firebaseio.com",
+  projectId: "train-scheduler-6b3c6",
+  storageBucket: "train-scheduler-6b3c6.appspot.com",
+  messagingSenderId: "614224330559"
+};
+firebase.initializeApp(config);
+
+var database = firebase(config);
+
