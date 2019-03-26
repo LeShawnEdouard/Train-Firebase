cba4c255bf130e27c748c3b7738e784f5000b999
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-03-26 00:11:08 -0400

    Added variables to store the value of the new train input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,3 +11,36 @@ firebase.initializeApp(config);
 
 var database = firebase(config);
 
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var trainTime = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+
+    var newTrain = {
+        name: trainName,
+        desti: destination,
+        time: trainTime,
+        rate: frequency
+    };
+
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.desti);
+    console.log(newTrain.time);
+    console.log(newTrain.rate);
+
+    
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+});
+
+
